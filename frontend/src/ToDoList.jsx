b4e54af6f80dafa96571fe3cee1dbc4ca0e75bb1
Petro <filipcenkopetro@gmal.com>
2025-06-19 22:13:49 +0300

    commit

--- frontend/src/ToDoList.jsx
+++ frontend/src/ToDoList.jsx
@@ -1,63 +1,84 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function ToDoList() {
     const [todos, setTodos] = useState([]);
-    const [text, setText] = useState("");
     const [editing, setEditing] = useState(null);
     const [editingText, setEditingText] = useState("");
+
     useEffect(() => {
         fetch('http://localhost:5000/api/todos')
             .then(res => res.json())
-            .then(data => setTodos(data))
-    }, [])
+            .then(data => setTodos(data));
+    }, []);
 
-    const addTodo = () => {
-        fetch('http://localhost:5000/api/todos', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({title: text}),
-        })
-            .then(res => res.json())
-            .then(newTodo => {
-                setTodos([...todos, newTodo]);
-                setText("")
-            });
-    }
+    const formik = useFormik({
+        initialValues: { title: "" },
+        validationSchema: Yup.object({
+            title: Yup.string()
+                .min(5, "Must be at least 5 characters")
+                .required("Required")
+        }),
+        onSubmit: (values, { resetForm }) => {
+            fetch('http://localhost:5000/api/todos', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ title: values.title })
+            })
+                .then(res => res.json())
+                .then(newTodo => {
+                    setTodos([...todos, newTodo]);
+                    resetForm();
+                });
+        }
+    });
+
     const deleteTodo = (id) => {
         fetch(`http://localhost:5000/api/todos/${id}`, {
             method: 'DELETE',
         })
             .then(res => res.json())
             .then(() => {
-                setTodos(todos.filter((todo) => todo._id !== id))
-            })
-    }
+                setTodos(todos.filter((todo) => todo._id !== id));
+            });
+    };
+
     const updateTodo = (id) => {
         fetch(`http://localhost:5000/api/todos/${id}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({title: editingText}),
-        }).then(res => res.json()).then((updateTodo) => {
-            setTodos(todos.map(todo => todo._id === id ? updateTodo : todo))
-            setEditing(null)
-            setEditingText("")
+            body: JSON.stringify({ title: editingText }),
         })
-    }
-
+            .then(res => res.json())
+            .then(updatedTodo => {
+                setTodos(todos.map(todo => todo._id === id ? updatedTodo : todo));
+                setEditing(null);
+                setEditingText("");
+            });
+    };
 
     return (
         <div>
             <h1>TODOLIST</h1>
-            <input
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                placeholder="Write some text here..."
-            />
-            <button onClick={addTodo}>Add</button>
+
+            <form onSubmit={formik.handleSubmit}>
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="Write some text here..."
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.title}
+                />
+                <button type="submit">Add</button>
+                {formik.touched.title && formik.errors.title ? (
+                    <div style={{ color: 'red' }}>{formik.errors.title}</div>
+                ) : null}
+            </form>
+
             <ol>
                 {todos.map((todo) => (
                     <li key={todo._id}>
@@ -68,24 +89,26 @@ function ToDoList() {
                                     onChange={(e) => setEditingText(e.target.value)}
                                 />
                                 <button onClick={() => updateTodo(todo._id)}>Save</button>
-                                <button onClick={() => { setEditing(null); setEditingText(""); }}>Cancel</button>
+                                <button onClick={() => {
+                                    setEditing(null);
+                                    setEditingText("");
+                                }}>Cancel</button>
                             </>
                         ) : (
                             <>
-                                <span className='text'>{todo.title}</span>
-                                <button onClick={() => deleteTodo(todo._id)} className='delete-btn'>Delete</button>
+                                <span className="text">{todo.title}</span>
+                                <button onClick={() => deleteTodo(todo._id)} className="delete-btn">Delete</button>
                                 <button onClick={() => {
                                     setEditing(todo._id);
                                     setEditingText(todo.title);
-                                }} className='edit-btn'>Edit</button>
+                                }} className="edit-btn">Edit</button>
                             </>
                         )}
-
                     </li>
                 ))}
             </ol>
         </div>
-    )
+    );
 }
 
-export default ToDoList
+export default ToDoList;
